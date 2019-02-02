5731540fc81a4ccea47022e1a65bbb1dae85bf04
Daniel Ruf <daniel@daniel-ruf.de>
2019-02-02 20:55:23 +0100

    chore: add bootstrap file and license

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+user="$USER"
+prefix="ssh-"
+username=${user#$prefix}
+userdir=/www/htdocs/${username}
+cd ${userdir}/bin
+curl -sSLo brotli.zip https://dl.bintray.com/eustas/brotli/brotli-linux-2018-11-12.zip
+unzip brotli.zip
+rm brotli.zip
+echo "alias brotli='${userdir}/bin/brotli'" >> ${userdir}/.bashrc
+source /.bashrc
+brotli --version
